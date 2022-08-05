7e6319a0fe963274dc88c8dd971325c51e0cc32f
shubham <shubhamvraut1995@gmail.com>
2022-08-05 14:02:25 +0530

    add fields in models.py

--- firstapp/models.py
+++ firstapp/models.py
@@ -6,3 +6,4 @@ class Event(models.Model):
     eventName=models.CharField(max_length=200)
     eventDate=models.CharField(max_length=100)
     area=models.CharField(max_length=100)
+    file=models.FileField(upload_to='/event')
