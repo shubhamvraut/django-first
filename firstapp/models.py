8ebcc5e9d1904b813198ac51340c11362fc12891
shubham <shubhamvraut1995@gmail.com>
2022-08-05 13:42:55 +0530

    add fields in models.py

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    eventName=models.CharField(max_length=200)
+    eventDate=models.CharField(max_length=100)
